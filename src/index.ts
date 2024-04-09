32ac69700334e40c2ae056d651451ca2a26c90da
atmarcosrodrigues <antonio.rodrigues.ct@gmail.com>
2024-04-09 14:52:09 -0300

    feat: Add databaseConnect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/databaseConnect";
+
+const HOST = process.env.SERVER_HOST;
+const PORT = process.env.SERVER_PORT;
 
 const app = express();
-const PORT = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello, TypeScript with Express!");
-});
+dotenv.config();
+connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at ${HOST}:${PORT}`);
 });
